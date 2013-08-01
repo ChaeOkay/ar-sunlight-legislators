04cc444113f6f95c43ea590d00383ce015f6a45b
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-01 14:38:03 -0700

    validations in politician.rb

--- app/models/politician.rb
+++ app/models/politician.rb
@@ -1,18 +1,8 @@
-# validation ideas
-##Formats
-# email
-# phone number
-#=>validates_length_of :phone in => 10..32,:allow_blank => true
-# website formats for various fields
-#=>validates_format_of :website,with=> URI::regexp(^(http|https))
-# twitter, facebook
-
-##Presence Of
-# title, firstname, lastname, party, state, in_office
-# => example: validates_presence_of :title
-#active record base => has initialize and save, etc.
 
 class Politician < ActiveRecord::Base
 
-
+  validates_presence_of :title, :firstname, :lastname, :party, :state, 
+                        :in_office
+  validates_length_of :phone in => 10..32, :allow_blank => true
+  validates_format_of :website, with => URI::regexp(%w(http https))
 end
