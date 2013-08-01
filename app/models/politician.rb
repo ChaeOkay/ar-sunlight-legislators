b1f23577dca13ca0ec0874721181be39d06d5eb1
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-01 15:09:59 -0700

    note on validations added

--- app/models/politician.rb
+++ app/models/politician.rb
@@ -2,7 +2,10 @@
 class Politician < ActiveRecord::Base
 
   validates_presence_of :title, :firstname, :lastname, :party, :state, 
-                        :in_office
-  validates_length_of :phone in => 10..32, :allow_blank => true
-  validates_format_of :website, with => URI::regexp(%w(http https))
+                        :in_office,
+  validates_length_of :phone, :in => 10..32, :allow_blank => true
+  validates_format_of :website, :with => URI::regexp(%w(http https))
+
+  # validiations will prevent new records from being saved
+  # if conditions are not met
 end
