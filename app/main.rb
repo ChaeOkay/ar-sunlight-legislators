4bb6f94a92566da468996bf85640b6c7a8dd4ec5
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-01 20:19:58 -0700

    count method added.

--- app/main.rb
+++ app/main.rb
@@ -17,4 +17,33 @@ def display_for_state(args)
   query(args[:state], args[:second_title])
 end
 
-display_for_state(:state => 'CA', :first_title => 'Sen', :second_title => 'Rep')
+def count(args)
+  Politician.where(args).count
+end
+
+arg1 = {:title => 'Sen'}
+arg2 = {:title => 'Sen', :gender => 'M'}
+arg3 = {:title => 'Rep'}
+arg4 = {:title => 'Rep', :gender => 'M'}
+
+p count(arg1)
+p count(arg2)
+p count(arg3)
+p count(arg4)
+
+# display_for_state(:state => 'CA', :first_title => 'Sen', :second_title => 'Rep')
+
+#2.
+# gender, title, in_office
+# come up with sequel query
+# => a = query total number of senators/representatives
+# => b = query number of male senators ----> count..
+# =>    AND in_office be = 1 *
+# divide 'b' by 'a' to find percentage, multiply 100. 
+# format for display. 
+
+# select title, count(title)  from politicians where title like '%Sen%'
+# group by title order by count(title) desc;
+
+
+# create a method to count the number of senators or representatives (arg is title)
--- app/models/politician.rb
+++ app/models/politician.rb
@@ -1,8 +1,8 @@
 
 class Politician < ActiveRecord::Base
 
-  validates_presence_of :title, :firstname, :lastname, :party, :state, 
-                        :in_office
+  validates_presence_of :title, :firstname, :lastname, :party, :state
+  validates_numericality_of :in_office, :equal_to => 1
   validates_length_of :phone, :in => 10..32, :allow_blank => true
   #validates_format_of :website, :with => URI::regexp(%w(http https))
 
