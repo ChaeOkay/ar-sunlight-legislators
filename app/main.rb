3c01c75b01a0a3ec57f79b2e7d619a8655c0e8d1
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-01 17:45:01 -0700

    sen rep query complete

--- app/main.rb
+++ app/main.rb
@@ -10,17 +10,43 @@ ca_sen_params = { :state => 'CA',
 
 ca_rep_params = { :state => 'CA', 
                   :title => 'Rep'}
-
-ca_senators = Politician.where(ca_sen_params)
-ca_representatives = Politician.where(ca_rep_params)
-
-ca_senators.each do |senator|
-  puts "#{senator[:firstname]} #{senator[:lastname]} (#{senator[:party]})"
-end 
-
-ca_representatives.each do |representative|
-  puts "#{representative[:firstname]} #{representative[:lastname]} (#{representative[:party]})"
+def query(params)
+  Politician.where(params).each do |object|
+    puts "\t#{object[:firstname]} #{object[:lastname]} (#{object[:party]})"
+  end
 end
+
+def display(sen_params, rep_params)
+  puts "Senators:"
+  query(sen_params)
+  puts "Representatives:"
+  query(rep_params)
+end
+
+display(ca_sen_params, ca_rep_params)
+
+# ca_senators = Politician.where(ca_sen_params)
+# ca_representatives = Politician.where(ca_rep_params)
+
+# ca_senators.each do |senator|
+#   puts "#{senator[:firstname]} #{senator[:lastname]} (#{senator[:party]})"
+# end 
+
+# ca_representatives.each do |representative|
+#   puts "#{representative[:firstname]} #{representative[:lastname]} (#{representative[:party]})"
+# end
+
+
+
+# create a method that ouputs formatted
+# create another method final output
+
+# final_result
+# puts "Sent"
+#   query(object(ca_senator, sen(params))
+#  puts "Rep"
+#   query(object, rep)
+#
 
 # run our query get Politicna objects
 # => Politician.where(:state => 'CA', :title => 'Sen.')
