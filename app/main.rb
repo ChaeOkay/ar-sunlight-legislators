6c6da3f8a29c4917093c6333c59d0fcce3bc0472
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-01 17:24:51 -0700

    search senators progress

--- app/main.rb
+++ app/main.rb
@@ -1,2 +1,22 @@
 # require class
 # write query on class object
+require_relative '../db/config.rb'
+require_relative 'models/politician.rb'
+
+#p Politician.where(:state => 'CA')
+
+ca_params = { :state => 'CA', 
+                :title => 'Sen'}
+
+ca_senators = Politician.where(ca_params)
+
+ca_senators.each do |senator|
+  puts "#{senator[:firstname]}"
+end
+
+# run our query get Politicna objects
+# => Politician.where(:state => 'CA', :title => 'Sen.')
+# object[:firstname]
+# object [:lastname]
+# object[:party]
+# format 
--- app/models/politician.rb
+++ app/models/politician.rb
@@ -4,7 +4,7 @@ class Politician < ActiveRecord::Base
   validates_presence_of :title, :firstname, :lastname, :party, :state, 
                         :in_office
   validates_length_of :phone, :in => 10..32, :allow_blank => true
-  validates_format_of :website, :with => URI::regexp(%w(http https))
+  #validates_format_of :website, :with => URI::regexp(%w(http https))
 
   # validiations will prevent new records from being saved
   # if conditions are not met
