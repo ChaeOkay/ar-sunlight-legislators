07327776e7fb181a2df809022fbd54615d35da29
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-01 17:29:32 -0700

    querying and display of name and party in main

--- app/main.rb
+++ app/main.rb
@@ -5,13 +5,21 @@ require_relative 'models/politician.rb'
 
 #p Politician.where(:state => 'CA')
 
-ca_params = { :state => 'CA', 
-                :title => 'Sen'}
+ca_sen_params = { :state => 'CA', 
+                  :title => 'Sen'}
 
-ca_senators = Politician.where(ca_params)
+ca_rep_params = { :state => 'CA', 
+                  :title => 'Rep'}
+
+ca_senators = Politician.where(ca_sen_params)
+ca_representatives = Politician.where(ca_rep_params)
 
 ca_senators.each do |senator|
-  puts "#{senator[:firstname]}"
+  puts "#{senator[:firstname]} #{senator[:lastname]} (#{senator[:party]})"
+end 
+
+ca_representatives.each do |representative|
+  puts "#{representative[:firstname]} #{representative[:lastname]} (#{representative[:party]})"
 end
 
 # run our query get Politicna objects
